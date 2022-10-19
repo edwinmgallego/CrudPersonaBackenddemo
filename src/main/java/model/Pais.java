5873f7e24395d51e1ab3c4430f746deb02c06cca
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-10-19 10:47:18 -0500

    CURSO SPRING BOOT CRUD + JPA + POSTGRESQL +REST + ANGULAR: Repositorios y servicios en Spring Boot

--- src/main/java/model/Pais.java
+++ src/main/java/model/Pais.java
@@ -10,4 +10,27 @@ public class Pais {
     private int id;
     private  String nombre;
 
+    public Pais() {
+    }
+
+    public Pais(String nombre) {
+        super();
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
